3f7eb90cbdfc1676b3635715968b6ae725e313ea
CrisLDev <leonardomoreirapazmio@gmail.com>
2021-07-07 22:42:57 -0500

    Added components and created router

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <span>Estamos en la vista home</span>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/views/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <span>Estamos en el componente login</span>
+        </div>
+    )
+}
+
+export default Login
